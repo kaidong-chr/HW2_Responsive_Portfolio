30d92cff5b7e4c778173664b8aec1f0c82e6b99a
Kai Dong <dongkai981@gmail.com>
2020-11-18 22:14:14 -0600

    Added on click function for the rest of the buttons and fixed some button classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,18 @@ document.querySelector(".beerConcierge").onclick = function () {
     const url = "https://github.com/kaidong-chr/The-Beer-Concierge";
     window.open(url);
   };
+
+document.querySelector(".weatherDashboard").onclick = function () {
+    const url = "https://github.com/kaidong-chr/HW6_Weather_Dashboard";
+    window.open(url);
+  };
+
+document.querySelector(".codeQuiz").onclick = function () {
+    const url = "https://github.com/kaidong-chr/HW4_Code_Quiz";
+    window.open(url);
+  };
+
+document.querySelector(".passwordGenerator").onclick = function () {
+    const url = "https://github.com/kaidong-chr/HW3_Password_Generator";
+    window.open(url);
+  };
